6422c91ceaa7e51c646c9c93041f6daf801b62be
antvirf <antti.viitala@icloud.com>
2025-04-04 14:22:47 +0800

    refactor: separate out `config` package

---
+++ internal/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"time"
+)
+
+var (
+	// All configuration options for `stui` are listed here
+	SearchDebounceInterval time.Duration
+	RefreshInterval        time.Duration
+	RequestTimeout         time.Duration
+	DebugMultiplier        int
+	SlurmBinariesPath      string
+	SlurmConfLocation      string
+	SlurmRestdAddress      string
+)
+
+func Configure() {
+	flag.DurationVar(&SearchDebounceInterval, "search-debounce-interval", 50, "interval in milliseconds to wait before searching")
+	flag.DurationVar(&RefreshInterval, "refresh-interval", 15, "interval in seconds when to refetch data")
+	flag.DurationVar(&RequestTimeout, "request-timeout", 4, "timeout setting for fetching data")
+	flag.IntVar(&DebugMultiplier, "debug-multiplier", 1, "multiplier for nodes and jobs, helpful when debugging and developing")
+	flag.StringVar(&SlurmBinariesPath, "slurm-binaries-path", "/usr/local/bin", "path where Slurm binaries like 'sinfo' and 'squeue' can be found")
+	flag.StringVar(&SlurmConfLocation, "slurm-conf-location", "/etc/slurm/slurm.conf", "path to slurm.conf for the desired cluster")
+	flag.StringVar(&SlurmRestdAddress, "slurm-restd-address", "", "URI for Slurm REST API if available, including protocol and port")
+	flag.Parse()
+
+	// Validate input and configs
+	if RequestTimeout > RefreshInterval {
+		log.Fatalf("Invalid arguments: request timeout of '%d' is longer than refresh interval of '%d'", RequestTimeout, RefreshInterval)
+	}
+
+	// Warnings about incomplete features
+	log.Printf("WARNING: flag value is currently unimplemented: slurm-binaries-path='%s'", SlurmBinariesPath)
+	log.Printf("WARNING: flag value is currently unimplemented: slurm-conf-location='%s'", SlurmConfLocation)
+	log.Printf("WARNING: flag value is currently unimplemented: slurm-restd-address='%s'", SlurmRestdAddress)
+}
--- main.go
+++ main.go
@@ -1,46 +1,31 @@
 package main
 
 import (
-	"flag"
-	"log"
 	"time"
 
+	"github.com/antvirf/stui/internal/config"
 	"github.com/antvirf/stui/internal/view"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	// Flags
-	var searchDebounceInterval = flag.Int("search-debounce-interval", 50, "interval in milliseconds to wait before searching")
-	var refreshInterval = flag.Int("refresh-interval", 15, "interval in seconds when to refetch data")
-	var requestTimeout = flag.Int("request-timeout", 4, "timeout setting for fetching data")
-	var debubMultiplier = flag.Int("debug-multiplier", 1, "multiplier for nodes and jobs, helpful when debugging and developing")
-	var slurmBinariesPath = flag.String("slurm-binaries-path", "/usr/local/bin", "path where Slurm binaries like 'sinfo' and 'squeue' can be found")
-	var slurmConfLocation = flag.String("slurm-conf-location", "/etc/slurm/slurm.conf", "path to slurm.conf for the desired cluster")
-	var slurmRestdAddress = flag.String("slurm-restd-address", "", "URI for Slurm REST API if available, including protocol and port")
-	flag.Parse()
-
-	// Validate input and configs
-	if *requestTimeout > *refreshInterval {
-		log.Fatalf("Invalid arguments: request timeout of '%d' is longer than refresh interval of '%d'", *requestTimeout, *refreshInterval)
-	}
-	log.Printf("WARNING: flag value is currently unimplemented: slurm-binaries-path='%s'", *slurmBinariesPath)
-	log.Printf("WARNING: flag value is currently unimplemented: slurm-conf-location='%s'", *slurmConfLocation)
-	log.Printf("WARNING: flag value is currently unimplemented: slurm-restd-address='%s'", *slurmRestdAddress)
-
+	config.Configure()
 	app := &view.App{
 		App:                    tview.NewApplication(),
 		Pages:                  tview.NewPages(),
-		RequestTimeout:         time.Duration(*requestTimeout) * time.Second, // Must be less than refreshInterval
-		DebugMultiplier:        *debubMultiplier,
-		SearchDebounceInterval: time.Duration(*searchDebounceInterval) * time.Millisecond,
+		RequestTimeout:         config.RequestTimeout * time.Second,
+		DebugMultiplier:        config.DebugMultiplier,
+		SearchDebounceInterval: config.SearchDebounceInterval * time.Millisecond,
 	}
 
 	app.SetupViews()
 	app.SetupKeybinds()
-	app.StartRefresh(time.Duration(*refreshInterval) * time.Second)
+	app.StartRefresh(config.RefreshInterval * time.Second)
 
-	if err := app.App.SetRoot(app.MainGrid, true).EnableMouse(false).Run(); err != nil {
+	if err := app.App.
+		SetRoot(app.MainGrid, true).
+		EnableMouse(false).
+		Run(); err != nil {
 		panic(err)
 	}
 }
