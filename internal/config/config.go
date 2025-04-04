9420a33a176ae8c557e0302e88fd341cfb1ff445
antvirf <antti.viitala@icloud.com>
2025-04-04 16:42:54 +0800

    feat: implement slurm-conf-location option

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"log"
+	"os"
 	"time"
 )
 
@@ -23,7 +24,7 @@ func Configure() {
 	flag.DurationVar(&RequestTimeout, "request-timeout", 4, "timeout setting for fetching data")
 	flag.IntVar(&DebugMultiplier, "debug-multiplier", 1, "multiplier for nodes and jobs, helpful when debugging and developing")
 	flag.StringVar(&SlurmBinariesPath, "slurm-binaries-path", "/usr/local/bin", "path where Slurm binaries like 'sinfo' and 'squeue' can be found")
-	flag.StringVar(&SlurmConfLocation, "slurm-conf-location", "/etc/slurm/slurm.conf", "path to slurm.conf for the desired cluster")
+	flag.StringVar(&SlurmConfLocation, "slurm-conf-location", "/etc/slurm/slurm.conf", "path to slurm.conf for the desired cluster, sets 'SLURM_CONF' environment variable")
 	flag.StringVar(&SlurmRestdAddress, "slurm-restd-address", "", "URI for Slurm REST API if available, including protocol and port")
 	flag.Parse()
 
@@ -32,6 +33,17 @@ func Configure() {
 	RefreshInterval = RefreshInterval * time.Second
 	RequestTimeout = RequestTimeout * time.Second
 
+	// If slurm.conf location was given, ensure file exists and configure env var if appropriate
+	if SlurmConfLocation != "" {
+		if _, err := os.Stat(SlurmConfLocation); err != nil {
+			log.Fatalf("Specified Slurm conf file cannot be found: %v", err)
+		}
+
+		if err := os.Setenv("SLURM_CONF", SlurmConfLocation); err != nil {
+			log.Fatalf("Failed to set SLURM_CONF environment variable: %v", err)
+		}
+	}
+
 	// Validate input and configs
 	if RequestTimeout > RefreshInterval {
 		log.Fatalf("Invalid arguments: request timeout of '%d' is longer than refresh interval of '%d'", RequestTimeout, RefreshInterval)
@@ -39,6 +51,5 @@ func Configure() {
 
 	// Warnings about incomplete features
 	log.Printf("WARNING: flag value is currently unimplemented: slurm-binaries-path='%s'", SlurmBinariesPath)
-	log.Printf("WARNING: flag value is currently unimplemented: slurm-conf-location='%s'", SlurmConfLocation)
 	log.Printf("WARNING: flag value is currently unimplemented: slurm-restd-address='%s'", SlurmRestdAddress)
 }
