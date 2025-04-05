16411da4c05eea87d9c57327ffbcb7f4dfb6d034
antvirf <antti.viitala@icloud.com>
2025-04-05 08:44:55 +0800

    fix: job launcher should launch jobs in all partitions

--- testing/test-job-launcher.sh
+++ testing/test-job-launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for NUMBER in {1..30}; do
     cat testing/mock-cluster-slurmconf.conf  | grep PartitionName | cut -d= -f2 | cut -d' ' -f1 | \
-        xargs -I{} sbatch --partition=physics --job-name=job-{}-$NUMBER --out=/dev/null testing/sleep.sh "$NUMBER"
+        xargs -I{} sbatch --partition={} --job-name=job-{}-$NUMBER --out=/dev/null testing/sleep.sh "$NUMBER"
 done
