d8d11456297653a73e2bb7d5a6870a9052fca4af
antvirf <antti.viitala@icloud.com>
2025-04-06 16:37:40 +0800

    refactor: remove repetition from keybinds.go

--- internal/view/keybinds.go
+++ internal/view/keybinds.go
@@ -6,7 +6,9 @@ import (
 	"time"
 
 	"github.com/antvirf/stui/internal/config"
+	"github.com/antvirf/stui/internal/model"
 	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 	"golang.design/x/clipboard"
 )
 
@@ -21,7 +23,7 @@ func (a *App) SetupKeybinds() {
 
 		switch event.Rune() {
 		case '?':
-			a.ShowKeyboardShorcutsModal()
+			a.ShowModalPopup("Shortcuts", config.KEYBOARD_SHORTCUTS)
 		case '1':
 			a.Pages.SwitchToPage("nodes")
 			a.FooterPaneLocation.SetText("[::b]Nodes (1)[::-] - Jobs (2) - Scheduler (3)")
@@ -58,28 +60,59 @@ func (a *App) SetupKeybinds() {
 	})
 
 	// Table view keybinds
-	a.NodesView.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	a.NodesView.SetInputCapture(
+		tableviewInputCapture(
+			a,
+			a.NodesView,
+			a.NodesTableData,
+			&a.SelectedNodes,
+			"NodeName", // Used for command modal
+			a.ShowNodeDetails,
+		),
+	)
+	a.JobsView.SetInputCapture(
+		tableviewInputCapture(
+			a,
+			a.JobsView,
+			a.JobsTableData,
+			&a.SelectedJobs,
+			"JobId", // Used for command modal
+			a.ShowJobDetails,
+		),
+	)
+}
+
+// Handles all inputs for table views (nodes and jobs)
+func tableviewInputCapture(
+	a *App,
+	view *tview.Table,
+	data *model.TableData,
+	selection *map[string]bool,
+	commandModalFilter string,
+	detailsFunction func(string),
+) func(*tcell.EventKey) *tcell.EventKey {
+	return func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Rune() {
 		case '/':
 			a.ShowSearchBox()
-			a.RerenderTableView(a.NodesView)
+			a.RerenderTableView(view)
 			a.App.SetFocus(a.SearchBox) // Only focus search when / is pressed
 			return nil
 		case ' ':
-			row, _ := a.NodesView.GetSelection()
+			row, _ := view.GetSelection()
 			if row > 0 { // Skip header row
-				nodeName := a.NodesView.GetCell(row, 0).Text
-				if a.SelectedNodes[nodeName] {
-					delete(a.SelectedNodes, nodeName)
+				entryName := view.GetCell(row, 0).Text
+				if (*selection)[entryName] {
+					delete(*selection, entryName)
 					// Set all cells in row to default background
-					for col := 0; col < a.NodesView.GetColumnCount(); col++ {
-						a.NodesView.GetCell(row, col).SetBackgroundColor(tcell.ColorBlack)
+					for col := 0; col < view.GetColumnCount(); col++ {
+						view.GetCell(row, col).SetBackgroundColor(tcell.ColorBlack)
 					}
 				} else {
-					a.SelectedNodes[nodeName] = true
+					(*selection)[entryName] = true
 					// Set all cells in row to orange background
-					for col := 0; col < a.NodesView.GetColumnCount(); col++ {
-						a.NodesView.GetCell(row, col).SetBackgroundColor(selectionColor)
+					for col := 0; col < view.GetColumnCount(); col++ {
+						view.GetCell(row, col).SetBackgroundColor(selectionColor)
 					}
 				}
 			}
@@ -88,26 +121,26 @@ func (a *App) SetupKeybinds() {
 			a.App.SetFocus(a.PartitionSelector)
 		case 'c':
 			// If user has a selection, use the selection
-			if len(a.SelectedNodes) > 0 {
-				a.ShowCommandModal("NodeName", a.SelectedNodes)
+			if len(*selection) > 0 {
+				a.ShowCommandModal(commandModalFilter, *selection)
 			} else {
 				// Otherwise, try to use the current node under the cursor, if any
-				row, _ := a.NodesView.GetSelection()
+				row, _ := view.GetSelection()
 				if row > 0 {
-					a.ShowCommandModal("NodeName", map[string]bool{
-						a.NodesView.GetCell(row, 0).Text: true,
+					a.ShowCommandModal(commandModalFilter, map[string]bool{
+						view.GetCell(row, 0).Text: true,
 					},
 					)
 				}
 			}
 			return nil
 		case 'y':
-			if len(a.SelectedNodes) > 0 {
+			if len(*selection) > 0 {
 				var sb strings.Builder
-				for nodeName := range a.SelectedNodes {
+				for entryName := range *selection {
 					// Find the node in our table data
-					for _, row := range a.NodesTableData.Rows {
-						if row[0] == nodeName { // NodeName is first column
+					for _, row := range data.Rows {
+						if row[0] == entryName { // NodeName is first column
 							if config.CopyFirstColumnOnly {
 								sb.WriteString(row[0])
 							} else {
@@ -119,9 +152,9 @@ func (a *App) SetupKeybinds() {
 					}
 				}
 				clipboard.Write(clipboard.FmtText, []byte(sb.String()))
-				count := len(a.SelectedNodes)
+				count := len(*selection)
 				a.ShowNotification(
-					fmt.Sprintf("[green]Copied %d selected node(s) to clipboard[white]", count),
+					fmt.Sprintf("[green]Copied %d selected row(s) to clipboard[white]", count),
 					2*time.Second,
 				)
 				return nil
@@ -130,131 +163,19 @@ func (a *App) SetupKeybinds() {
 
 		switch event.Key() {
 		case tcell.KeyEnter:
-			row, _ := a.NodesView.GetSelection()
+			row, _ := view.GetSelection()
 			if row > 0 { // Skip header row
-				nodeName := a.NodesView.GetCell(row, 0).Text
-				a.ShowNodeDetails(nodeName)
+				entryName := view.GetCell(row, 0).Text
+				detailsFunction(entryName)
 				return nil
 			}
 		case tcell.KeyEsc:
 			if a.SearchActive {
 				a.HideSearchBox()
-				a.RerenderTableView(a.NodesView)
+				a.RerenderTableView(view)
 				return nil
 			}
 		}
 		return event
-	})
-
-	a.JobsView.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Rune() {
-		case '/':
-			a.ShowSearchBox()
-			a.RerenderTableView(a.JobsView)
-			a.App.SetFocus(a.SearchBox) // Only focus search when / is pressed
-			return nil
-		case ' ':
-			row, _ := a.JobsView.GetSelection()
-			if row > 0 { // Skip header row
-				jobID := a.JobsView.GetCell(row, 0).Text
-				if a.SelectedJobs[jobID] {
-					delete(a.SelectedJobs, jobID)
-					// Set all cells in row to default background
-					for col := 0; col < a.JobsView.GetColumnCount(); col++ {
-						a.JobsView.GetCell(row, col).SetBackgroundColor(tcell.ColorBlack)
-					}
-				} else {
-					a.SelectedJobs[jobID] = true
-					// Set all cells in row to orange background
-					for col := 0; col < a.JobsView.GetColumnCount(); col++ {
-						a.JobsView.GetCell(row, col).SetBackgroundColor(selectionColor)
-					}
-				}
-			}
-			return nil
-		case 'p':
-			a.App.SetFocus(a.PartitionSelector)
-		case 'c':
-			// If user has a selection, use the selection
-			if len(a.SelectedJobs) > 0 {
-				a.ShowCommandModal("JobId", a.SelectedJobs)
-			} else {
-				// Otherwise, try to use the current row under the cursor, if any
-				row, _ := a.JobsView.GetSelection()
-				if row > 0 {
-					a.ShowCommandModal("JobId", map[string]bool{
-						a.JobsView.GetCell(row, 0).Text: true,
-					},
-					)
-				}
-			}
-			return nil
-		case 'y':
-			if len(a.SelectedJobs) > 0 {
-				var sb strings.Builder
-				for jobID := range a.SelectedJobs {
-					// Find the job in our table data
-					for _, row := range a.JobsTableData.Rows {
-						if row[0] == jobID { // JobID is first column
-							if config.CopyFirstColumnOnly {
-								sb.WriteString(row[0])
-							} else {
-								sb.WriteString(strings.Join(row, " "))
-							}
-							sb.WriteString(config.CopiedLinesSeparator)
-							break
-						}
-					}
-				}
-				clipboard.Write(clipboard.FmtText, []byte(sb.String()))
-				count := len(a.SelectedJobs)
-				a.ShowNotification(
-					fmt.Sprintf("[green]Copied %d selected job(s) to clipboard[white]", count),
-					5*time.Second,
-				)
-				return nil
-			}
-		}
-
-		switch event.Key() {
-		case tcell.KeyEnter:
-			row, _ := a.JobsView.GetSelection()
-			if row > 0 { // Skip header row
-				jobID := a.JobsView.GetCell(row, 0).Text
-				a.ShowJobDetails(jobID)
-				return nil
-			}
-		case tcell.KeyEsc:
-			if a.SearchActive {
-				a.HideSearchBox()
-				a.RerenderTableView(a.JobsView)
-				return nil
-			}
-		}
-		return event
-	})
-
-	// Search box keybinds
-	a.SearchBox.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Key() {
-		case tcell.KeyEsc:
-			a.HideSearchBox()
-			a.RerenderTableView(a.CurrentTableView)
-			return nil
-		case tcell.KeyEnter:
-			if a.SearchPattern == "" {
-				a.HideSearchBox()
-			} else {
-				a.App.SetFocus(a.CurrentTableView)
-			}
-			return nil
-		}
-		return event
-	})
-	// Node detail view keybinds are set in ShowNodeDetails()
-}
-
-func (a *App) ShowKeyboardShorcutsModal() {
-	// START_SHORTCUTS
-	a.ShowModalPopup("Shortcuts", config.KEYBOARD_SHORTCUTS)
+	}
 }
