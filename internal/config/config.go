b98a4b723f2533328a78498fd36dda5cea1b6965
antvirf <antti.viitala@icloud.com>
2025-04-08 21:13:28 +0800

    release: bump to 0.0.3

--- internal/config/config.go
+++ internal/config/config.go
@@ -28,7 +28,7 @@ var (
 )
 
 const (
-	STUI_VERSION       = "0.0.2"
+	STUI_VERSION       = "0.0.3"
 	KEYBOARD_SHORTCUTS = `General Shortcuts
 1        Switch to Nodes view
 2        Switch to Jobs view
