45a0bcba962fcde11fbf736f89e01977191ec52a
antvirf <antti.viitala@icloud.com>
2025-04-12 09:12:42 +0800

    style: thin borders

--- internal/view/tview_defaults.go
+++ internal/view/tview_defaults.go
@@ -4,18 +4,18 @@ import "github.com/rivo/tview"
 
 func init() {
 	// Focused borders
-	tview.Borders.HorizontalFocus = tview.BoxDrawingsHeavyHorizontal
-	tview.Borders.VerticalFocus = tview.BoxDrawingsHeavyVertical
-	tview.Borders.TopLeftFocus = tview.BoxDrawingsHeavyDownAndRight
-	tview.Borders.TopRightFocus = tview.BoxDrawingsHeavyDownAndLeft
-	tview.Borders.BottomLeftFocus = tview.BoxDrawingsHeavyUpAndRight
-	tview.Borders.BottomRightFocus = tview.BoxDrawingsHeavyUpAndLeft
+	tview.Borders.HorizontalFocus = tview.BoxDrawingsLightHorizontal
+	tview.Borders.VerticalFocus = tview.BoxDrawingsLightVertical
+	tview.Borders.TopLeftFocus = tview.BoxDrawingsLightDownAndRight
+	tview.Borders.TopRightFocus = tview.BoxDrawingsLightDownAndLeft
+	tview.Borders.BottomLeftFocus = tview.BoxDrawingsLightUpAndRight
+	tview.Borders.BottomRightFocus = tview.BoxDrawingsLightUpAndLeft
 
 	// Non-focus borders
-	tview.Borders.BottomLeft = tview.BoxDrawingsHeavyUpAndRight
-	tview.Borders.BottomRight = tview.BoxDrawingsHeavyUpAndLeft
-	tview.Borders.TopRight = tview.BoxDrawingsHeavyDownAndLeft
-	tview.Borders.TopLeft = tview.BoxDrawingsHeavyDownAndRight
-	tview.Borders.Horizontal = tview.BoxDrawingsHeavyHorizontal
-	tview.Borders.Vertical = tview.BoxDrawingsHeavyVertical
+	tview.Borders.BottomLeft = tview.BoxDrawingsLightUpAndRight
+	tview.Borders.BottomRight = tview.BoxDrawingsLightUpAndLeft
+	tview.Borders.TopRight = tview.BoxDrawingsLightDownAndLeft
+	tview.Borders.TopLeft = tview.BoxDrawingsLightDownAndRight
+	tview.Borders.Horizontal = tview.BoxDrawingsLightHorizontal
+	tview.Borders.Vertical = tview.BoxDrawingsLightVertical
 }
