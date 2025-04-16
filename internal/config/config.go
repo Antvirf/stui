6ce6caf43a817cc42686e09c645f98e0cd54129a
antvirf <antti.viitala@icloud.com>
2025-04-16 20:17:36 +0800

    release: 0.0.8

--- internal/config/config.go
+++ internal/config/config.go
@@ -38,7 +38,7 @@ var (
 )
 
 const (
-	STUI_VERSION       = "0.0.7"
+	STUI_VERSION       = "0.0.8"
 	KEYBOARD_SHORTCUTS = `General Shortcuts
 1        Switch to Nodes view
 2        Switch to Jobs view
