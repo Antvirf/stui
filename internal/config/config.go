0fd2a7a5274d972307229d8b7b5230ae430f7547
antvirf <antti.viitala@icloud.com>
2025-09-14 11:53:45 +0800

    feat(selectors: add `Ctrl+A` (all) and `Ctrl+I` (invert) selection shortcuts
    Also adds '| %d selected' to the title bar. This is done in a rather
    ugly and code-repeating way, lots of room here to refactor towards some
    central notification/text sharing channel. Regex filter works
    differently as it has to render a new table every time - the selector
    does not, and I did not want to induce a full render on every select
    either just to keep the code simpler. Instead there should be some
    better way to send that info between components, likely a central
    notifications channel. TBC.

--- internal/config/config.go
+++ internal/config/config.go
@@ -69,6 +69,8 @@ k/j      Move selection up/down in table view
 h/l      Scroll left/right in table view
 Arrows   Scroll up/down/left/right in table view
 ?        Show this help
+Ctrl+A   Select all currently filtered rows
+Ctrl+I   Invert selection within filtered rows
 Ctrl+R   Refresh currently visible data
 Ctrl+C   Exit
 o        Sort table by column
