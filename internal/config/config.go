03ae99739e035aac04c70577dea026425ad3c711
antvirf <antti.viitala@icloud.com>
2026-02-02 13:24:34 +0800

    release: 0.12.0

--- internal/config/config.go
+++ internal/config/config.go
@@ -61,7 +61,7 @@ var (
 )
 
 const (
-	STUI_VERSION       = "0.11.5"
+	STUI_VERSION       = "0.12.0"
 	KEYBOARD_SHORTCUTS = `GENERAL SHORTCUTS
 1        Switch to Nodes view (scontrol)
 2        Switch to Jobs view (scontrol)
